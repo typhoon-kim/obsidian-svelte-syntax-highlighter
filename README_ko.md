3e52f4771a30b088cd5c03bcd6b2941717ea0432
typhoon.kim <typh007.kim@gmail.com>
2024-05-31 20:29:47 +0900

    읽기모드prism svelte 구문 추가

---
+++ README_ko.md
@@ -0,0 +1,49 @@
+# **옵시디언 Svelte 하이라이터 플러그인**
+
+[English](https://typhoon-kim.github.io/obsidian-svelte-syntax-highlighter/) | 한국어
+
+[![npm 버전](https://badgen.net/npm/v/@typh007/obsidian-svelte-highlighter)](https://www.npmjs.com/package/@typh007/obsidian-svelte-highlighter)
+[![다운로드](https://badgen.net/npm/dt/@typh007/obsidian-svelte-highlighter)](https://www.npmjs.com/package/@typh007/obsidian-svelte-highlighter)
+[![jsDeliver](https://data.jsdelivr.com/v1/package/npm/@typh007/obsidian-svelte-highlighter/badge)](https://www.jsdelivr.com/package/npm/@typh007/obsidian-svelte-highlighter)
+
+## 설치
+
+```bash
+npm i @typh007/obsidian-svelte-highlighter
+```
+
+## 사용법
+
+```javascript
+
+```
+
+```javascript
+
+```
+
+```javascript
+
+```
+
+## 결과
+
+```javascript
+
+```
+
+## 🔭 향후 기능 추가
+
+- [ ] **<>**
+
+## 🤝 기여
+
+컨트리뷰션 요청이 있다면 환영합니다!
+
+## 📝 라이선스
+
+이 프로젝트는 [MIT 라이선스](LICENSE) 하에 배포됩니다.
+
+---
+
+*추가적인 질문이나 지원이 필요한 경우 언제든지 문의 주세요!*
--- main.ts
+++ main.ts
@@ -1,33 +1,38 @@
-import { MarkdownPostProcessor, MarkdownPostProcessorContext, Plugin, loadPrism } from 'obsidian';
-import { Decoration, DecorationSet, EditorView, ViewPlugin, ViewUpdate } from '@codemirror/view';
-import { RangeSetBuilder } from '@codemirror/state';
+import { App, MarkdownPostProcessorContext, Plugin, PluginManifest, loadPrism } from 'obsidian';
 import 'prism-svelte';
 
-let globalPrism: any;
+let obsidianPrism: any;
 
 export default class SvelteSyntaxHighlightingPlugin extends Plugin {
-    private markdownProcessor: MarkdownPostProcessor;
+    
+    constructor (app: App, manifast: PluginManifest) {
+        super(app, manifast);
+        // obsidianPrism = await loadPrism().then(prism => {
+        //     obsidianPrism = prism;
+        // }).catch(error => {
+        //     console.error('Failed to load Prism: ', error);
+        // });
+    }
 
     async onload() {
-        console.log('Loading Svelte Syntax Highlighting Plugin');
-
         try {
-            // Prism.js 로드
-            globalPrism = await loadPrism();
-    
+            console.log('Loading Svelte Syntax Highlighting Plugin');
+            obsidianPrism = await loadPrism();
+            console.log(obsidianPrism);
+
             // Svelte 구문 강조 설정
-            if (!globalPrism.languages.svelte) {
+            if (!obsidianPrism.languages.svelte) {
                 console.log("add svelte to prismjs");
 
-                globalPrism.languages.svelte = {
-                    ...globalPrism.languages.markup,
+                obsidianPrism.languages.svelte = {
+                    ...obsidianPrism.languages.markup,
                     'script': {
                         pattern: /<script[\s\S]*?>[\s\S]*?<\/script>/i,
                         inside: {
                             'language-javascript': {
                                 pattern: /(<script[\s\S]*?>)[\s\S]*?(<\/script>)/i,
                                 lookbehind: true,
-                                inside: globalPrism.languages.javascript
+                                inside: obsidianPrism.languages.javascript
                             }
                         }
                     },
@@ -37,7 +42,7 @@ export default class SvelteSyntaxHighlightingPlugin extends Plugin {
                             'language-css': {
                                 pattern: /(<style[\s\S]*?>)[\s\S]*?(<\/style>)/i,
                                 lookbehind: true,
-                                inside: globalPrism.languages.css
+                                inside: obsidianPrism.languages.css
                             }
                         }
                     }
@@ -45,98 +50,25 @@ export default class SvelteSyntaxHighlightingPlugin extends Plugin {
             }
 
             // Markdown Post Processor 등록 (읽기 모드)
-            this.markdownProcessor = this.registerMarkdownPostProcessor((element: HTMLElement, context: MarkdownPostProcessorContext) => {
+            this.registerMarkdownPostProcessor((element: HTMLElement, context: MarkdownPostProcessorContext) => {
                 element.querySelectorAll('pre > code.language-svelte').forEach((block) => {
-                    globalPrism.highlightElement(block);
+                    obsidianPrism.highlightElement(block);
                 });
             });
-
-            // CodeMirror 플러그인 등록 (편집 모드)
-            this.registerEditorExtension(ViewPlugin.fromClass(SvelteEditorPlugin, {
-                decorations: (plugin) => plugin.decorations,
-            }));
 
         } catch (error) {
             console.log('Faild to load Prism: ', error);
         }
-
-        console.log(globalPrism);
     }
 
     onunload() {
         console.log('Unloading Svelte Syntax Highlighting Plugin');
 
         // Prism.js에서 Svelte 구문 제거
-        if (globalPrism.languages.svelte) {
-            console.log("remove svelte to prismjs");
-            delete globalPrism.languages.svelte;
+        if (obsidianPrism && obsidianPrism.languages.svelte) {
+            delete obsidianPrism.languages.svelte;
+            console.log("remove svelte from prismjs");
+            console.log(obsidianPrism.languages.svelte);
         }
-
-        // Markdown Post Processor 제거
-        this.app.workspace.off('markdown-post-process', this.markdownProcessor);
-
-        console.log(globalPrism);
-    }
-}
-
-class SvelteEditorPlugin {
-    decorations: DecorationSet;
-
-    constructor(view: EditorView) {
-        this.decorations = this.buildDecorations(view);
-    }
-
-    update(update: ViewUpdate) {
-        if (update.docChanged || update.viewportChanged) {
-            this.decorations = this.buildDecorations(update.view);
-        }
-    }
-
-    destroy() {
-        // 필요한 경우 자원 정리 코드 추가
-    }
-
-    buildDecorations(view: EditorView): DecorationSet {
-        const builder = new RangeSetBuilder<Decoration>();
-
-        for (const { from, to } of view.visibleRanges) {
-            const text = view.state.doc.sliceString(from, to);
-            const codeBlockRegex = /```svelte([\s\S]*?)```/g;
-            let match;
-
-            while ((match = codeBlockRegex.exec(text.toLowerCase())) !== null) {
-                const code = match[1];
-                const start = from + match.index + 9; // 9는 "```svelte"의 길이
-
-                // Prism.highlight를 사용하여 코드 하이라이팅
-                const highlighted = globalPrism.highlight(code, globalPrism.languages.svelte, 'svelte');
-                const tempDiv = document.createElement("div");
-                tempDiv.innerHTML = highlighted;
-
-                let pos = start;
-                tempDiv.childNodes.forEach((child) => {
-                    if (child.nodeType === Node.TEXT_NODE) {
-                        const textNode = child as Text;
-                        if (textNode.textContent) {
-                            pos += textNode.textContent.length;
-                        }
-                    } else if (child.nodeType === Node.ELEMENT_NODE) {
-                        const elementNode = child as HTMLElement;
-                        const textContent = elementNode.innerText;
-                        if (textContent) {
-                            const className = elementNode.className.split(" ").join(".");
-                            const deco = Decoration.mark({
-                                class: className,
-                                tagName: elementNode.tagName.toLowerCase()
-                            });
-                            builder.add(pos, pos + textContent.length, deco);
-                            pos += textContent.length;
-                        }
-                    }
-                });
-            }
-        }
-
-        return builder.finish();
     }
 }
