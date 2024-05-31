e2a9b440c1599979f583ee8360dfa2b88e7c32ac
typhoon.kim <typh007.kim@gmail.com>
2024-05-31 22:39:48 +0900

    prism with svelte 모듈 적용

--- main.ts
+++ main.ts
@@ -1,63 +1,29 @@
-import { App, MarkdownPostProcessorContext, Plugin, PluginManifest, loadPrism } from 'obsidian';
-import 'prism-svelte';
-
-let obsidianPrism: any;
+import loadPrismWithSvelte from 'loadPrismWithSvelte';
+import { App, MarkdownPostProcessorContext, Plugin, PluginManifest } from 'obsidian';
 
 export default class SvelteSyntaxHighlightingPlugin extends Plugin {
-    
-    constructor (app: App, manifast: PluginManifest) {
-        super(app, manifast);
-        // obsidianPrism = await loadPrism().then(prism => {
-        //     obsidianPrism = prism;
-        // }).catch(error => {
-        //     console.error('Failed to load Prism: ', error);
-        // });
+    obsidianPrism: any;
+
+    constructor(app: App, manifest: PluginManifest) {
+        super(app, manifest);
     }
 
     async onload() {
         try {
             console.log('Loading Svelte Syntax Highlighting Plugin');
-            obsidianPrism = await loadPrism();
-            console.log(obsidianPrism);
+            this.obsidianPrism = await loadPrismWithSvelte();
 
-            // Svelte 구문 강조 설정
-            if (!obsidianPrism.languages.svelte) {
-                console.log("add svelte to prismjs");
-
-                obsidianPrism.languages.svelte = {
-                    ...obsidianPrism.languages.markup,
-                    'script': {
-                        pattern: /<script[\s\S]*?>[\s\S]*?<\/script>/i,
-                        inside: {
-                            'language-javascript': {
-                                pattern: /(<script[\s\S]*?>)[\s\S]*?(<\/script>)/i,
-                                lookbehind: true,
-                                inside: obsidianPrism.languages.javascript
-                            }
-                        }
-                    },
-                    'style': {
-                        pattern: /<style[\s\S]*?>[\s\S]*?<\/style>/i,
-                        inside: {
-                            'language-css': {
-                                pattern: /(<style[\s\S]*?>)[\s\S]*?(<\/style>)/i,
-                                lookbehind: true,
-                                inside: obsidianPrism.languages.css
-                            }
-                        }
-                    }
-                };
-            }
+            console.log(this.obsidianPrism);
 
             // Markdown Post Processor 등록 (읽기 모드)
             this.registerMarkdownPostProcessor((element: HTMLElement, context: MarkdownPostProcessorContext) => {
                 element.querySelectorAll('pre > code.language-svelte').forEach((block) => {
-                    obsidianPrism.highlightElement(block);
+                    this.obsidianPrism.highlightElement(block);
                 });
             });
 
         } catch (error) {
-            console.log('Faild to load Prism: ', error);
+            console.log('Failed to load Prism: ', error);
         }
     }
 
@@ -65,10 +31,9 @@ export default class SvelteSyntaxHighlightingPlugin extends Plugin {
         console.log('Unloading Svelte Syntax Highlighting Plugin');
 
         // Prism.js에서 Svelte 구문 제거
-        if (obsidianPrism && obsidianPrism.languages.svelte) {
-            delete obsidianPrism.languages.svelte;
-            console.log("remove svelte from prismjs");
-            console.log(obsidianPrism.languages.svelte);
+        if (this.obsidianPrism && this.obsidianPrism.languages.svelte) {
+            delete this.obsidianPrism.languages.svelte;
+            console.log("Removed Svelte from Prism.js");
         }
     }
 }
